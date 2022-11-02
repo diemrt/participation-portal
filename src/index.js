228e6a5079125630067eed43640df09ee1a00581
diego <diego.martignoni@abenergie.it>
2022-11-02 18:34:10 +0100

    mocking entry point

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <ErrorPage />,
     errorElement: <ErrorPage />
   },
 ]);
